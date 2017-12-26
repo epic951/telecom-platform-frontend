5fc3380ad700d8a3ce8169fb3a9ed693f05e3524
epic951 <inertialism@gmail.com>
2017-12-26 08:22:49 +0200

    modified dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { DataService } from './data.service';
 import { AppComponent } from './app.component';
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule ,
+    BrowserModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [DataService],
