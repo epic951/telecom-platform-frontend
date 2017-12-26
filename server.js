da15aec6979b05fd750e1b1792e4c3b3f68f5010
epic951 <inertialism@gmail.com>
2017-12-26 12:05:47 +0200

    modified GET routing

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
     , cors = require('cors')
-    , app = express();
+    , app = express(), path = require('path');
 
 app.use(cors());
 
@@ -9,4 +9,9 @@ app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+
 console.log('server listening ' + process.env.PORT);
